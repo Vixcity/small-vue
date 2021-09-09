133734796df8a881260c7d30935c84b8b3dff04c
Vixcity <2091283625@qq.com>
2021-09-09 19:01:47 +0800

    09/09

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,13 @@
 class ReactiveEffect {
   private _fn: any;
 
-  constructor(fn) {
+  constructor(fn,public scheduler?) {
     this._fn = fn;
   }
 
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -35,13 +35,22 @@ export function trigger(target, key) {
   let dep = depsMap.get(key);
 
   for (const effect of dep) {
-    effect.run();
+    if(effect.scheduler){
+      effect.scheduler()
+    }else {
+
+      effect.run();
+    }
   }
 }
 
 let activeEffect;
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn,options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
 
   _effect.run();
+
+  const runner = _effect.run.bind(_effect)
+
+  return runner
 }
--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -18,4 +18,57 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+
+  it("should return runner when call effect", () => {
+    // effect -> function(runner) -> fn -> return
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+
+  it("scheduler", () => {
+    // 1. 通过 effect 的 第二个参数给定的一个 scheduler 的 fn
+    // 2. 当 effect 第一次执行的时候，还会执行 fn
+    // 3. 当响应式对象 set updata 的时候，不会执行 fn ，而是执行 scheduler
+    // 4. 当执行 runner 的时候，会再次执行 fn
+    let dummy;
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    
+    // should be called on first trigger
+    // 应该在第一次触发时调用吗
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+
+    // should not run yet
+    // 还不应该运行
+    expect(dummy).toBe(1);
+
+    // manually run
+    // 手动运行
+    run();
+    
+    // should have run
+    // 应该运行
+    expect(dummy).toBe(2);
+  });
 });
