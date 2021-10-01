58ecdfb007f3365e3af214aaadf7437525640ab9
Vixcity <2091283625@qq.com>
2021-10-01 15:22:51 +0800

    2021-09-01 15:22:04 星期五 更新 准备写 porcessElement 和 mountElement

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -9,13 +10,17 @@ function patch(vnode, container) {
   // 处理组件
 
   // TODO:判断这个vnode是不是element类型?处理element:处理component
-  // 思考题：如何去区分是element类型还是component类型
+  // 思考题：如何去区分是element类型还是component类型 -> 判断他的type是不是对象
   // processElement()
-
-  processComponent(vnode, container);
+  isObject(vnode.type) ? processComponent(vnode, container) : processElement(vnode)
 }
+
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container);
+}
+
+function processElement(vnode: any) {
+  mountElement(vnode);
 }
 
 function mountComponent(vnode: any, container) {
@@ -24,6 +29,11 @@ function mountComponent(vnode: any, container) {
   setupComponent(instance);
   setupRenderEffect(instance, container);
 }
+
+function mountElement(vnode: any) {
+  // ...
+}
+
 
 function setupRenderEffect(instance: any, container) {
   const subTree = instance.render();
